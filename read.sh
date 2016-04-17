fd4cecc9db88fa3d35dd8d9c701ce2f5db31b6d1
su-kun1899 <higedrum.coz@gmail.com>
2016-04-18 03:24:32 +0900

    TIL 対話処理

---
+++ read.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while :
+do
+  read key
+  echo "you pressed $key"
+  if [ $key = "end" ]; then
+    break
+  fi
+done
+
+select option in CODE DIE
+do
+  echo "you pressed $option"
+  break
+done
